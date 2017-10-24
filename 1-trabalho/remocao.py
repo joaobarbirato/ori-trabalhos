ca778cdbf649eabd13931caa57f79c5e189ed0bf
joaobarbirato <joaobarbirato@gmail.com>
2017-10-24 15:23:37 -0200

    inicio de insercao e remocao

---
+++ 1-trabalho/remocao.py
@@ -0,0 +1,29 @@
+TAM_REGISTRO = 100
+TAM_REG_INDICE = 50
+# dado o registro
+def remocao1(lf, reg):
+    # verificar onde está no arquivo de índice
+        # busca:
+    indice = lf.getIndex()
+    indice.seek(0, 0)
+    achei = False
+    while(indice.read(TAM_REG_INDICE) == None and !achei): # loop do arquivo de índice
+        indice.seek(-TAM_REG_INDICE, 1) # em relação ao atual
+        i = indice.read(TAM_REGISTRO)
+        lf.seek(0,0)
+        r = lf.read(TAM_REGISTRO * i.getRRN() + lf.getHeader().getSize(), 0) # em relação ao primeiro
+        if(r = reg):
+            achei = True
+    # retorno
+    if(!achei)
+        print("Nao há esse registro.")
+    else: # se achei o registro
+        lf.getHeader().push(i.getRRN()) # atualiza a lista invertida
+        return reg
+
+
+# dado o RRN
+def remocao2(lf, RRN):
+    lf.seek(TAM_REGISTRO * RNN + lf.getHeader().getSize(), 0)
+    lf.getHeader().push(RRN)
+    return lf.read(TAM_REGISTRO)
