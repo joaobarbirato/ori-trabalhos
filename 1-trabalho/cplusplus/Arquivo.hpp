83bcb7d7691f001d1e89fe613326fea2c6001c29
joaobarbirato <joaobarbirato@gmail.com>
2017-10-29 11:53:39 -0200

    insercao, busca e listagem, tratando dos arquivos de dados e de indice

--- 1-trabalho/cplusplus/Arquivo.hpp
+++ 1-trabalho/cplusplus/Arquivo.hpp
@@ -23,12 +23,14 @@ class Arquivo{
         FILE * dados;
         ArquivoIndice * indice;
         RegistroCabecalho rcabecalho;
+        int ultimoRRN;
     public:
         Arquivo(string nomeArquivo);
         ~Arquivo();
         int getTopo();
-        void insere(const Registro *);
+        void insere(const Registro &);
         void lista();
+        bool busca(const char *, Registro &);
 };
 
 Arquivo::Arquivo(string nomeArquivo): nome(nomeArquivo){
@@ -37,6 +39,8 @@ Arquivo::Arquivo(string nomeArquivo): nome(nomeArquivo){
     this->rcabecalho.topo = -1;
     fwrite(&this->rcabecalho, TAM_REG_CABECALHO, 1, this->dados);
     fclose(this->dados);
+
+    this->ultimoRRN = 0;
 };
 
 Arquivo::~Arquivo(){
@@ -45,27 +49,63 @@ Arquivo::~Arquivo(){
 
 int Arquivo::getTopo(){ return rcabecalho.topo; };
 
-void Arquivo::insere(const Registro * reg){
-    char arroba;
+void Arquivo::insere(const Registro & reg){
     int rrn;
     dados = fopen(nome.c_str(), "r+b");
     // verificar se há espaços vazios
     if(rcabecalho.topo == -1){ // se não há espaços vazios
         fseek(dados, 0, SEEK_END);
-        fwrite(reg, TAM_REG, 1, dados);
+        fwrite(&reg, TAM_REG, 1, dados);
+        indice->insere(reg.cpf, ultimoRRN);
+        ultimoRRN++;
     }else{ // se há
         fseek(dados, TAM_REG * rcabecalho.topo + TAM_REG_CABECALHO + sizeof('@'), SEEK_SET); // vá ao topo da lista
         fread(&rrn, sizeof(int), 1, dados); // guarde o próximo da lista (novo topo)
         rcabecalho.topo = rrn;
         fseek(dados, -(sizeof('@')+sizeof(int)), SEEK_CUR); // volte o ponteiro
-        fwrite(reg, TAM_REG, 1, dados); // escreva o registro
+        fwrite(&reg, TAM_REG, 1, dados); // escreva o registro
     }
     fclose(dados);
 };
 
 void Arquivo::lista(){
+    Registro rAux;
+    int k;
+    char ehArroba;
 
+    dados = fopen(nome.c_str(), "rb");
+    fseek(dados, TAM_REG_CABECALHO, SEEK_SET);
+    // exibição sequencial dos dadoss
+    while(1 == fread(&rAux, TAM_REG, 1, dados)){ // se não chegou no fim do arquivo binário
+        // verificar se o registro lido está removido
+        fread(&ehArroba, sizeof(char),1,dados);
+        fseek(dados, -sizeof(char), SEEK_CUR);
+        if(ehArroba != '@'){ // se nao achar '@', não está removido
+            cout << endl;
+            cout << rAux.nome << endl;
+            cout << rAux.cpf << endl;
+            cout << rAux.idade << endl;
+            cout << endl;
+        }else{ // se está removido
+            cout << endl << "REMOVIDO" << endl << endl << endl << endl;
+        }
+    }
+    fclose(dados);
 };
-    
+
+bool Arquivo::busca(const char * chave, Registro & R){
+    //TODO: Tratar string com tamanho menor
+    int posicao = indice->busca(chave);
+    if(posicao == -1) // se nao achou
+        return false; 
+    else{ // se achou
+        dados = fopen(nome.c_str(), "rb");
+        fseek(dados, TAM_REG_CABECALHO + TAM_REG * posicao, SEEK_SET); // vá até a posicao
+        fread(&R, TAM_REG, 1, dados); // leia o registro e passe-o por referência
+        fclose(dados);
+    }
+    return true;
+};
+
 
 #endif
--- 1-trabalho/cplusplus/ArquivoIndice.hpp
+++ 1-trabalho/cplusplus/ArquivoIndice.hpp
@@ -1,24 +1,52 @@
 #ifndef ARQUIVOINDICE
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 typedef struct{
-    char cpf[11];
+    char chave[12];
     int rrn;
-} registroIndice;
+} RegistroIndice;
 
-const int TAM_REG_INDICE = 15;
+const int TAM_REG_INDICE = sizeof(RegistroIndice);
 
 class ArquivoIndice{
 	private:
-    	FILE * arqIndice;	
+		const string nome;
+    	FILE * arquivo;
     public:
     	ArquivoIndice(string nome);
+    	void insere(const char *, const int &);
+    	int busca(const char *);
 };
 
-ArquivoIndice::ArquivoIndice(string nome){
-	this->arqIndice = fopen((nome + "-indice").c_str(), "wb");
-	fclose(this->arqIndice);
+ArquivoIndice::ArquivoIndice(string nomeArquivo): nome(nomeArquivo + "-indice"){
+	this->arquivo = fopen((nomeArquivo + "-indice").c_str(), "wb");
+	fclose(this->arquivo);
+};
+
+void ArquivoIndice::insere(const char * cpf, const int &  posicao){
+	RegistroIndice * rAux = new RegistroIndice;// criando um novo registro de índice
+	// preenchendo o novo registro de índice
+	sprintf(rAux->chave, "%s", cpf);
+	fflush(stdin);
+	rAux->rrn = posicao;
+	// adicionando no arquivo de índice
+	arquivo = fopen(nome.c_str(), "ab"); // append
+	fwrite(rAux, TAM_REG_INDICE, 1, arquivo);
+	fclose(arquivo);
+	delete rAux;
+};
+
+int ArquivoIndice::busca(const char * cpf){
+	RegistroIndice rAux;
+
+	arquivo = fopen(nome.c_str(), "rb");
+	while(1 == fread(&rAux, TAM_REG_INDICE, 1, arquivo)) // busque sequencialmente no indice
+		if(!strcmp(rAux.chave, cpf)) // se forem iguais (achou a chave)
+			return rAux.rrn; // retorne o rrn da chave
+	
+	return -1; // senao, retorne -1
 };
 
 #endif
